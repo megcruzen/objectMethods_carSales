444f7aa4b2688edd0f567e7d0de644a43b05a061
Meg Cruzen <megcruzen@gmail.com>
2019-01-09 19:55:36 -0600

    Added car keys and values to DOM

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,14 +7,19 @@ Randall has come back with a new job for you. Each week, he wants to see a repor
 */
 
 
-// 1. Display the first and last name of the sales agent.
-
 let container = document.querySelector("#container");
 container.innerHTML = "<h1>Weekly Sales Report</h1>"
 
 salesByWeek.forEach(sale => {
+    container.innerHTML += "<hr />";
 
     let agentName = `${sale.sales_agent.first_name} ${sale.sales_agent.last_name}`;
-    container.innerHTML += `<h3>${agentName}</h3`;
+    container.innerHTML += `<h3>${agentName}</h3>`;
+
+    /* For...Of with Object.entries() | Prints both key and value */
+    // console.log(Object.entries(sale));
+    for (const entry of Object.entries(sale.vehicle)) {
+        container.innerHTML += `<div>${entry[0]}: ${entry[1]}</div>`
+    }
 
 })
